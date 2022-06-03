f529e2a1746d974a97d2f1191f29d4370a586b34
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-06-03 12:12:44 +0300

    Added new Methods.

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const axios = require("axios").default
+
+
+
 class RexarTools{
     constructor(){
 
+    }
+    /**
+     * 
+     * @param {any} val 
+     * @returns {boolean}
+     */
+    isBigInt(val){
+        return typeof val === "bigint"
+    }
+    /**
+     * 
+     * @param {any} val 
+     * @returns {boolean}
+     */
+    isNumber(val){
+        return typeof val === "number"
     }
     /**
      * 
@@ -72,6 +92,47 @@ class RexarTools{
      */
     stringify(val){
         return this.isString(val) ? val : String(val)
+    }
+    /**
+     * 
+     * @param {string} pkgName 
+     * @returns {Promise<import("./typings/Types").NPMReturns>}
+     */
+    async npm(pkgName){
+        const resp = await axios.request({
+            url: "https://registry.npmjs.com/" + pkgName
+        })
+        const data = resp.data
+        const v = data["dist-tags"].latest
+        return {
+            name: data.name,
+            version: v,
+            description: data.versions[v].description,
+            keywords: data.versions[v].keywords,
+            createdDate: data.time.created,
+            modifiedDate: data.time.modified,
+            repository: data.versions[v].repository,
+            bugs: data.versions[v].bugs,
+            homepage: data.versions[v].homepage,
+            author: data.versions[v].author.name,
+            license: data.versions[v].license
+        }
+    }
+    /**
+     * 
+     * @param {any} val 
+     * @returns {number}
+     */
+    numberify(val){
+        return this.isNumber(val) ? val : Number(val)
+    }
+    /**
+     * 
+     * @param {string | number | bigint | boolean} val 
+     * @returns {bigint}
+     */
+    bigingify(val){
+        return this.isBigInt(val) ? val : BigInt(val)
     }
 }
 
